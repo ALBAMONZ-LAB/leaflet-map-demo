631622fc5f150f304e8f4702cd0a6093ba52737c
이승원 <skmb1230@naver.com>
2025-01-21 00:22:00 +0900

    Fix eslint 사용하지 않은 변수 waring 처리

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // 기본 no-unused-vars 규칙 비활성화
+      "no-unused-vars": "off",
+      // @typescript-eslint/no-unused-vars 규칙 설정
+      "@typescript-eslint/no-unused-vars": [
+        "warn", // 경고로 처리
+        {
+          vars: "all", // 모든 변수를 확인
+          args: "none", // 함수 매개변수는 확인하지 않음
+          varsIgnorePattern: "^_", // _로 시작하는 변수는 무시
+          argsIgnorePattern: "^_", // _로 시작하는 매개변수는 무시
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
